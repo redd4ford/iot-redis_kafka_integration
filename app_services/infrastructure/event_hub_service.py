c5a40c43b0b4456b4600825e2518a8bf59992bce
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-06-15 06:08:58 +0300

    Added services for Redis and Event Hub logging

--- app_services/infrastructure/event_hub_service.py
+++ app_services/infrastructure/event_hub_service.py
@@ -1,3 +1,33 @@
+import asyncio
+import json
+from typing import (
+    List,
+    Union,
+)
+
+from azure.eventhub.aio import EventHubProducerClient
+from azure.eventhub import EventData
+from django.conf import settings
+
+from injector import inject
+
+
 class EventHubService:
-    # TODO(redd4ford): Event Hub integration
-    pass
+    @inject
+    def __init__(self, producer: EventHubProducerClient = settings.EVENT_HUB):
+        self.producer = producer
+        super().__init__()
+
+    async def send_data(self, data: Union[List[dict], dict]):
+        async with self.producer:
+            if isinstance(data, list):
+                event_data_batch = await self.producer.create_batch()
+                for row in data:
+                    event_data_batch.add(EventData(json.dumps(row)))
+                    await self.producer.send_batch(event_data_batch)
+
+            elif isinstance(data, dict):
+                await self.producer.send_event(EventData(json.dumps(data)))
+
+
+loop = asyncio.get_event_loop()
--- app_services/infrastructure/redis_service.py
+++ app_services/infrastructure/redis_service.py
@@ -1,30 +1,23 @@
 from django.conf import settings
+from injector import inject
 from redis.client import Redis
-
-from app_services.logger import context
 
 
 class RedisService:
+    @inject
     def __init__(self, redis_server: Redis = settings.REDIS_SERVER):
         self.redis = redis_server if redis_server else None
+        super().__init__()
 
     def is_processed(self, link: str) -> bool:
-        # TODO(redd4ford): remove these and make the app drop when there is no Redis server
-        if self.redis:
-            return self.redis.get(link) is not None
-        else:
-            return False
+        return self.get(link) is not None
 
     def set(self, key: str, value: str) -> None:
-        if self.redis:
-            self.redis.set(key, value)
+        print(key, value)
+        self.redis.hset('file', key, value)
 
-    def get(self, key: str) -> None:
-        if self.redis:
-            self.redis.get(key)
+    def get(self, key: str):
+        return self.redis.hget('file', key)
 
     def log_status(self, data: dict):
-        if self.redis:
-            self.set(**data)
-            if not settings.WRITE_TO_KAFKA:
-                context.write_log(**data)
+        self.set(**data)
