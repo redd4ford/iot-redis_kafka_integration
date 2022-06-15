0cea0b9b44e13db8e59694c8cfe48fe089deb813
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-06-15 14:32:30 +0300

    Added Redis status conversion to string

--- app_services/infrastructure/redis_service.py
+++ app_services/infrastructure/redis_service.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.conf import settings
 from injector import inject
 from redis.client import Redis
@@ -16,8 +18,12 @@ class RedisService:
         print(key, value)
         self.redis.hset('file', key, value)
 
-    def get(self, key: str):
-        return self.redis.hget('file', key)
+    def get(self, key: str) -> Optional[str]:
+        status = self.redis.hget('file', key)
+        if status:
+            return status.decode("utf-8")
+        else:
+            return None
 
     def log_status(self, data: dict):
         self.set(**data)
