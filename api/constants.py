fa34d1c3d6de0443049d36b2f7e0d79c7152e0a5
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-06-15 14:33:18 +0300

    Removed redundant code

--- api/constants.py
+++ api/constants.py
@@ -11,39 +11,3 @@ class FileStatus:
     N_RECORDS_UPLOADED = lambda number_of_records_uploaded: f"{number_of_records_uploaded} records uploaded"
     DONE = "Done"
     ALREADY_PROCESSED = "Already processed"
-
-    @classmethod
-    def _build_status(cls, link_to_file: str, status: str) -> dict:
-        """
-        Return processing status for a specific file as a key-value pair suitable for Redis
-        caching.
-        """
-        return {"key": link_to_file, "value": status}
-
-    @classmethod
-    def in_progress(cls, link_to_file: str) -> dict:
-        """
-        Build IN_PROGRESS status for a file.
-        """
-        return cls._build_status(link_to_file, status=FileStatus.IN_PROGRESS)
-
-    @classmethod
-    def n_records_uploaded(cls, link_to_file: str, counter: int) -> dict:
-        """
-        Build N_RECORDS_UPLOADED status for a file.
-        """
-        return cls._build_status(link_to_file, status=FileStatus.N_RECORDS_UPLOADED(counter))
-
-    @classmethod
-    def done(cls, link_to_file: str) -> dict:
-        """
-        Build DONE status for a file.
-        """
-        return cls._build_status(link_to_file, status=FileStatus.DONE)
-
-    @classmethod
-    def already_processed(cls, link_to_file: str) -> dict:
-        """
-        Build ALREADY_PROCESSED status for a file.
-        """
-        return cls._build_status(link_to_file, status=FileStatus.ALREADY_PROCESSED)
