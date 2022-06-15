487f8b435e2bb7ab6ac8712b4b4f4a04fab6cd7d
Viktoriia Yehorova <egorova.vik@gmail.com>
2022-06-15 06:10:46 +0300

    Added Event Hub logging, updated console logging strategy

--- app_services/logger/logger.py
+++ app_services/logger/logger.py
@@ -2,48 +2,44 @@ from abc import (
     ABC,
     abstractmethod,
 )
-from typing import Union
+from typing import (
+    Union,
+    List,
+)
 
-from django.conf import settings
-
-# TODO(redd4ford): implement logging with built-in logging lib
 from django.utils import timezone
+from injector import inject
+
+from app_services.infrastructure import EventHubService
 
 
 class Logger(ABC):
     @abstractmethod
-    def log(self, data: dict):
+    def log(self, data: dict, is_status: bool = False):
         pass
-
-
-class Context:
-    def __init__(self, logger: Logger):
-        self.logger = logger
-
-    def write_log(self, data: Union[dict, str]) -> None:
-        self.logger.log(data)
 
 
 class ConsoleLogger(Logger):
     def __init__(self):
-        print("Console Logging")
+        print("Console Logging enabled")
 
     def log(self, data: dict, is_status: bool = False) -> None:
+        # TODO(redd4ford): implement logging with built-in logging lib
         if is_status:
-            print(f"{timezone.now()} --- [{data.keys()}] = {data.values()}")
+            print(f"{timezone.now()} --- [{data['key']}] = {data['value']}")
         else:
-            print(f"[{timezone.now()}] --- #{data['counter']} [{data['link']}] --- {data['body']}")
+            print(
+                f"[{timezone.now()}] --- {data['link']} --- "
+                f"#{data['counter']} --- {data['body']} --- "
+            )
 
 
 class EventHubLogger(Logger):
-    def __init__(self):
-        print("Event Hub Logging")
+    @inject
+    def __init__(self, service: EventHubService = EventHubService()):
+        self.service = service
+        super().__init__()
+        print("Event Hub Logging enabled")
 
-    def log(self, data: dict) -> None:
-        print(f"{data['body']}")
-
-
-context = Context(
-    EventHubLogger() if settings.WRITE_TO_KAFKA
-    else ConsoleLogger()
-)
+    async def log(self, data: Union[List[dict], dict], is_status: bool = False) -> None:
+        await self.service.send_data(data)
